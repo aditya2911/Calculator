9915d5a1588d36067b08b738dfc1a103533dff75
Aditya Salunke <adisalu15@gmail.com>
2021-12-06 00:47:34 +0530

    added changes so that inputed number would be send in a array , decimal pronlem and operation problem send into array need to be fixed

--- script1.js
+++ script1.js
@@ -1,41 +1,251 @@
-class Calculator{
-    constructor(previousOperandTextElement,currentOperandTextElement){
+
+let index = [];
+let mulIndex;
+let operand1;
+let operand2;
+let mul;
+let result = 1;
+
+let workingarr = [];
+
+let currenNum = "";
+let flag = 0;
+let indicator = 0;
+
+function replaceAt(groupofchar, index, replacement) {
+    console.log("apna index " + index);
+    let tempAttCr = [];
+    let iValue = index - 2;
+    console.log("RePLACEMENT " + replacement);
+    tempAttCr = Array.from(groupofchar.toString());
+    console.log("apna array" + tempAttCr);
+    let tempReplacementholder = replacement.toString();
+
+    for (let h = iValue; h <= index; h++) {
+        let consoleArr = [];
+        console.log("i is " + h);
+        consoleArr.push(tempAttCr[h]);
+        console.log("CONSOLEAR " + consoleArr);
+
+        delete tempAttCr[h];
+        console.log("currentCOndtion " + tempAttCr);
+
+
+    }
+    console.log("tempAttCR " + tempAttCr);
+    tempAttCr[index - 1] = tempReplacementholder;
+    console.log("thisis indexc " + index)
+    console.log("tempAttCR " + tempAttCr);
+
+
+    let temp1 = [];
+
+    for (let j of tempAttCr) {
+        j && temp1.push(j);
+
+    }
+    console.log("temp1" + temp1.length);
+    console.log('this is temp1' + temp1);
+
+    let tempString = '';
+    for (let y = 0; y < temp1.length; y++) {
+        console.log("temp1 values " + temp1[y]);
+        tempString = tempString + temp1[y].toString();
+        console.log('we iteratubng' + tempString)
+    }
+    console.log('tempoString' + tempString.length);
+    let test = Array.from(tempString);
+    console.log('Test' + test);
+    return tempString;
+
+}
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
     }
 
-    clear(){
+    clear() {
         this.currentOperand = '';
-        this.previousOperandText= ''
+        this.previousOperandText = ''
         this.operation = undefined;
+        workingarr = new Array;
+        console.log("THanos "+workingarr);
+
     }
 
-    delete(){
-        this.currentOperand = this.currentOperand.toString().slice(0,-1);
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+        
+        
+       (currenNum =="")?"v":workingarr.push(currenNum);
+            if(workingarr.at(-1)!=''){
+                let indexElement = workingarr.at(-1);
+                console.log("indexELement"+indexElement)
+                let changedElement = indexElement.toString().slice(0,-1);
+                workingarr[workingarr.length-1] = changedElement.toString();
+
+                if(changedElement =='')workingarr.pop();
+                console.log("changed man"+changedElement);
+                console.log("length:"+workingarr.length);
+                }
+            else{
+                console.log("why are u here");
+                workingarr.pop();
+            }
+            currenNum = "";
+        
+       
+
+        console.log("pop pop "+workingarr);
     }
 
-    appendNumber(number){
+    appendNumber(number) {
         console.log(number);
-        if(number == '÷' &&  this.currentOperand == '') {console.log("he;l");return;}
-        if(number == '×' &&  this.currentOperand == '') {console.log("he;l");return;}
-        if(number == '%' &&  this.currentOperand == '') {console.log("he;l");return;}
-        else{
-        if(number == "." && this.currentOperand.includes("."))return;
-        this.currentOperand = this.currentOperand.toString() +number.toString();
-    }}
-    chooseOperation(operation){
+        if (number == '÷' && this.currentOperand == '') { console.log("he;l"); return; }
+        if (number == '×' && this.currentOperand == '') { console.log("he;l"); return; }
+        if (number == '%' && this.currentOperand == '') { console.log("he;l"); return; }
+
+
+
+        if(number == '( )'){
+            if(indicator == 1){
+            if(currenNum!="")workingarr.push(currenNum);
+            this.currentOperand = this.currentOperand.toString() + ")"
+            indicator =0;
+            currenNum = ""
+            }
+            else{
+                if(currenNum!="")workingarr.push(currenNum);
+                this.currentOperand = this.currentOperand.toString() + "("
+                indicator =1;
+                currenNum = ""
+            }
+        }
+
+        else {
+            if (number == "." && this.currentOperand.includes(".")) return;
+            this.currentOperand = this.currentOperand.toString() + number.toString();
+        }
+
+        console.log((number));
+        if (number == '+' || number == '%' || number == '×' || number == '÷' || number == '-') return;
+       
+        if (number == '( )') {
+            console.log("inside flag")
+          
+            if (flag == 1) {
+                workingarr.push(")")
+                flag = 0;
+            }
+            else{
+                workingarr.push("(");
+                flag = 1;
+            }
+        }
+        if(number!="( )"){
+        if (number == '.' && currenNum.includes('.')) return;
+        currenNum += number;
+        }
+
+    }
+    chooseOperation(operation) {
         this.operation = operation;
         this.previousOperand = this.currentOperand;
         this.currentOperand = "";
     }
-    compute(){
-        console.log(this.currentOperand);
-        let output =  Function(this.currentOperand);
-        console.log(output);
-        outputScreen.value = output.toString();
+
+    pushNumber(operation) {
+
+
+        workingarr.push(currenNum);
+        workingarr.push(operation);
+
+
+        if (operation == '-' && workingarr.length == 0) {
+            workingarr.push(operation);
+        }
+        currenNum = "";
+        console.log("we are working here" + workingarr);
     }
-    updateDisplay(){
+
+    compute() {
+
+        let storeString = this.currentOperand.toString();
+        let operatedString = this.currentOperand.toString();
+
+        for (let b = 0; b < storeString.length; b++) {
+            if (operatedString.includes('×'))
+                console.log("hi " + operatedString)
+
+            {
+                let indexOfMutiSign = operatedString.indexOf('×');
+                console.log("index:" + indexOfMutiSign)
+                if (indexOfMutiSign != -1) {
+                    let arrayExample = [];
+                    arrayExample = Array.from(operatedString);
+
+                    let operandi1 = arrayExample[indexOfMutiSign - 1];
+                    let operandi2 = arrayExample[indexOfMutiSign + 1];
+                    console.log(operandi1 + "operan1");
+                    console.log(operandi2 + "operan2");
+                    let result1 = operandi1 * operandi2;
+                    let tempIndex = indexOfMutiSign + 1;
+                    operatedString = replaceAt(operatedString, tempIndex, result1);
+                    console.log("tumharta " + operatedString)
+                    console.log("this is result" + result1);
+                    currentOperandTextElement1.value = operatedString.toString();
+                }
+            }
+
+
+        }
+
+
+        let tempArr = this.currentOperand.toString();
+        for (let i = 0; i < tempArr.length; i++) {
+            if (tempArr[i] == '×') index.push(i + 1); {
+                // console.log('in')
+                for (let a = 0; a < index.length; a++) {
+                    mul = index[a];
+
+                    operand1 = (tempArr.charAt(mulIndex - 1));
+                    operand2 = (tempArr.charAt(mulIndex + 1));
+                    result = operand1 * operand2;
+                    this.currentOperand.c
+                    result = temp * result;
+                    // console.log("multiplication" + result);
+                }
+                // index.forEach((xIndex) => {
+                //      mulIndex = xIndex ;
+                //      operand1 = tempArr.charAt(mulIndex - 1);
+                //      operand2 = tempArr.charAt(mulIndex + 1);
+                //     result = operand1 * operand2
+                //     console.log("multiplication" + result);
+
+                // })
+
+            }
+
+            if (tempArr.includes("+")) {
+                console.log('inAdd')
+                let mulIndex = tempArr.indexOf('+');
+                let operand1 = tempArr.charAt(mulIndex - 1);
+                let operand2 = tempArr.charAt(mulIndex + 1);
+                result = operand1 + operand2
+                console.log("addition" + result);
+            }
+        }
+
+
+        // console.log(tempArr + "sup");
+        // let output =
+        //     // console.log(output);
+        //     outputScreen.value = output.toString();
+    }
+    updateDisplay() {
         // outputScreen.style.width = "3.5rem"/this.currentOperand.length ;
         // outputScreen.style.maxWidth = "10ch";
 
@@ -56,37 +266,42 @@ const equalButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement1= document.querySelector('[data-current-operand]');
+const currentOperandTextElement1 = document.querySelector('[data-current-operand]');
 
 
 
-const  calculator = new Calculator(previousOperandTextElement,currentOperandTextElement1);
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement1);
 
-numberButtons.forEach(button =>{
- button.addEventListener("click",()=>{
-     calculator.appendNumber(button.innerText);
-     calculator.updateDisplay();
- })})
-
- operationButton.forEach(button =>{
-    button.addEventListener("click",()=>{
-        console.log(button.innerText)
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
         calculator.appendNumber(button.innerText);
         calculator.updateDisplay();
-    })})
+    })
+})
 
-    equalButton.addEventListener('click',()=>{
-        calculator.compute();
-        calculator.clear();
-    });
-    
-    deleteButton.addEventListener('click',()=>{
-        calculator.delete();
+operationButton.forEach(b1 => {
+    b1.addEventListener("click", () => {
+        // console.log(button.innerText)
+        calculator.appendNumber(b1.innerText);
         calculator.updateDisplay();
-       })
+        calculator.pushNumber(b1.innerText);
+    })
+})
 
-       allClearButton.addEventListener('click',()=>{
+equalButton.addEventListener('click', () => {
+    calculator.compute();
+    workingarr.push(currenNum);
+    console.log("you pressed equal "+workingarr);
+    calculator.clear();
+});
 
-        calculator.clear();
-        calculator.updateDisplay();
-       })
+deleteButton.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', () => {
+
+    calculator.clear();
+    calculator.updateDisplay();
+})
